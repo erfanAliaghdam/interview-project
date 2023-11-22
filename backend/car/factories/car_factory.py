a674fa0dbd2a0393502f519a30a26e12c7bf6f9e
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2023-11-23 00:14:19 +0330

    cars seed and factory

---
+++ backend/car/factories/car_factory.py
@@ -0,0 +1,17 @@
+from car.models import Car
+import factory
+
+
+class CarFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Car
+
+    title = factory.Faker('word')
+    description = factory.Faker('sentence')
+    color = factory.Faker(
+        'random_element',
+        elements=[choice[0] for choice in Car.COLOR_CHOICES]
+    )
+    carrying_capacity = factory.Faker('random_int', min=1, max=24)
+    cylinder_number = factory.Faker('random_int', min=1, max=16)
+    cylinder_capacity = factory.Faker('random_int', min=50, max=660)
