dbe36bfd50f9e50281bbafc21593e692149d9c0b
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2023-11-22 11:47:07 +0330

    sale group seeds and adminsite

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -3,13 +3,16 @@ from django.contrib.auth import get_user_model
 from users.adminsites import (
     ClientUserAdmin,
     AdminUserAdmin,
+    SellerUserAdmin
 )
 from users.models.proxy import (
     ClientProxyModel,
     AdminProxyModel,
+    SaleProxyModel
 )
 
 admin.site.register(get_user_model())
 admin.site.register(ClientProxyModel, ClientUserAdmin)
 admin.site.register(AdminProxyModel, AdminUserAdmin)
+admin.site.register(SaleProxyModel, SellerUserAdmin)
 
--- backend/users/models/proxy/author_user_proxy_model.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import get_user_model
-
-
-class AuthorProxyModel(get_user_model()):
-    class Meta:
-        proxy = True
-        verbose_name = "Author user"
-        verbose_name_plural = "Author users"
